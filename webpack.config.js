8bbe6d7c793337df145535d7298718501a6451bc
鲍伟杰 <baoweijie@xin.com>
2018-05-03 16:17:51 +0800

    官网—> 代码分离 章节—> 防止重复splitChunks，沿用的 起步 和 管理输出 章节的代码

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,25 @@
 const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
 
 module.exports = {
     entry: {
-        app: "./src/index.js",
-        print: "./src/print.js"
+        index: "./src/index.js",
+        another: "./src/another-module.js"
     },
     plugins: [
-        new CleanWebpackPlugin(['dist']),
-        new HtmlWebpackPlugin({
-            title: "Output Management"
+        new CleanWebpackPlugin(["dist"]),
+        new HTMLWebpackPlugin({
+            title: "Code Splitting"
         })
     ],
+    optimization: {
+		splitChunks: {
+			minSize: 1,
+            chunks: "initial",
+			name:"common"  // 指定公共 bundle 的名称。
+		}
+	},
     output: {
         filename: "[name].bundle.js",
         path: path.resolve(__dirname, "dist")
