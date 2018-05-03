83cb54ff4f84a2cb88e987e614667d711edb7319
鲍伟杰 <baoweijie@xin.com>
2018-05-03 18:26:06 +0800

    官网—> 代码分离 章节—> 动态导入，沿用的 起步 和 管理输出 章节的代码

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 async function getComponent() {
-    var element = document.createElement("div");
-    const _ = await import(/* webpackChunkName: "lodash" */ "lodash");
-    element.innerHTML = _.join(["Hello", "webpack"], " ");
-    return element;
-}
+    var element = document.createElement('div');
+       const _ = await import(/* webpackChunkName: "lodash" */ 'lodash');
+    
+       console.log('111222_.join_', _)
+       console.log('element', element)
 
+    //    官网上没有加 default ，default 是自己加的，因为打印 _ 有问题，加上就好用了
+    //    element.innerHTML = _.join(['Hello', 'webpack'], ' ');       
+       element.innerHTML = _.default.join(['Hello', 'webpack'], ' ');
+    
+       return element;
+}
 getComponent().then(component => {
+    console.log("component", component)
     document.body.appendChild(component);
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,13 @@
 const path = require("path");
 const HTMLWebpackPlugin = require("html-webpack-plugin");
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
     entry: {
         index: "./src/index.js"
     },
     plugins: [
+        new CleanWebpackPlugin(["dist"]),
         new HTMLWebpackPlugin({
             title: "Code Splitting"
         })
