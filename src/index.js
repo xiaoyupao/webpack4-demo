87b35564a1cf30a277f616b55657f9fd1b9fd448
鲍伟杰 <baoweijie@xin.com>
2018-05-23 14:06:33 +0800

    官网—> shimming 章节—> shimming 全局变量，沿用的 起步 章节的代码

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
-import _ from 'lodash';
-
-function component() {
+  function component() {
     var element = document.createElement('div');
-  
-    // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
+
+    // element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.innerHTML = join(['Hello', 'webpack'], ' ');
+
     return element;
   }
-  
+
   document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,16 @@
-const path = require('path');
+const path = require("path");
+const webpack = require("webpack");
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  }
+    entry: "./src/index.js",
+    plugins: [
+        new webpack.ProvidePlugin({
+            //    _: 'lodash'
+            join: ["lodash", "join"]
+        })
+    ],
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, "dist")
+    }
 };
